e38c68f1bbb21498a26fc58f0b6860534948bc93
Tien Dung <dungtn@gmail.com>
2023-04-01 13:04:48 +0700

    kịch bản huấn luyện cuối

--- lora-run.sh
+++ lora-run.sh
@@ -1,11 +1,11 @@
 [ -f vi_merged.jsonl ] || cat vi*.jsonl > vi_merged.jsonl
 
-deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path vi_alpaca_reduced.jsonl
+#deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path vi_alpaca_reduced.jsonl
 
-#deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json --output_dir './chat-gpt-neo-1.3B-5e' --lora_r 16 \
-#	--data_path './vi_alpaca_reduced.jsonl' --batch_size 180 --per_device_batch_size 20 --num_epochs 2
+deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json --output_dir './chat-gpt-neo-1.3B_merged-3e' --lora_r 16 \
+	--data_path './vi_merged.jsonl' --batch_size 180 --per_device_batch_size 20 --num_epochs 3
 
 #deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json --output_dir './chat-gpt-neo-1.3B-3e' --lora_r 16 \
 #        --data_path './sample.jsonl' --batch_size 180 --per_device_batch_size 20 --num_epochs 3 --val_set_size=20
 
-# deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path sample.jsonl --val_set_size=20 --resume_from_checkpoint checkpoint-200
+#deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path sample.jsonl --val_set_size=20 --resume_from_checkpoint checkpoint-200

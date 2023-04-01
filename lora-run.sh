d6458bfbb56354633125144e873c8d5a46d52f49
Tien Dung <dungtn@gmail.com>
2023-04-01 07:12:59 +0700

    Add sample data and sample training to test the whole process fast

--- lora-run.sh
+++ lora-run.sh
@@ -1,4 +1,5 @@
 [ -f vi_merged.jsonl ] || cat vi*.jsonl > vi_merged.jsonl
 
-# https://huggingface.co/docs/transformers/main_classes/deepspeed
 deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path vi_alpaca_reduced.jsonl
+
+# deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path sample.jsonl --val_set_size=20 --resume_from_checkpoint checkpoint-200

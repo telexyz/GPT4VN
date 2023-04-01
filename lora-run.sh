794d345487c9edb1328c0b1f48f2984b32c0e665
Tien Dung <dungtn@gmail.com>
2023-04-01 12:48:42 +0700

    Use deepspeed stage 2 to stablize training

--- lora-run.sh
+++ lora-run.sh
@@ -2,7 +2,10 @@
 
 deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path vi_alpaca_reduced.jsonl
 
-deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json \
-	--data_path vi_alpaca_reduced.jsonl --batch_size 256 --per_device_batch_size 32
+#deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json --output_dir './chat-gpt-neo-1.3B-5e' --lora_r 16 \
+#	--data_path './vi_alpaca_reduced.jsonl' --batch_size 180 --per_device_batch_size 20 --num_epochs 2
+
+#deepspeed --num_gpus=3 lora.py --deepspeed ds_z3_bf16_config.json --output_dir './chat-gpt-neo-1.3B-3e' --lora_r 16 \
+#        --data_path './sample.jsonl' --batch_size 180 --per_device_batch_size 20 --num_epochs 3 --val_set_size=20
 
 # deepspeed --num_gpus=1 lora.py --deepspeed ds_z3_bf16_config.json --data_path sample.jsonl --val_set_size=20 --resume_from_checkpoint checkpoint-200

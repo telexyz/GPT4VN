c5ee93090d36b5784ae8a80995598c15cacb3a69
Tien Dung <dungtn@gmail.com>
2023-04-02 20:30:53 +0700

    fix

--- finetune-run.sh
+++ finetune-run.sh
@@ -3,7 +3,7 @@
 ###############################
 [ -f vi_merged.jsonl ] || cat vi*.jsonl > vi_merged.jsonl
 
-python3 finetune.py --data_path 'vi_alpaca_reduced.jsonl' \
+python3 finetune.py --data_path 'vi_merged.jsonl' \
     --batch_size=128 --micro_batch_size 2 --num_epochs 1 --output_dir 'chat-gpt-neo-1.3B-merged-1e' \
     # --bf16 False --fp16 True # <= để chạy trên Google Colab (hoặc GPUs đời cũ)
 

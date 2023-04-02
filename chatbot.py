c22c12c9fce20bb70ac2e950097f350ec290dd8e
Tien Dung <dungtn@gmail.com>
2023-04-02 22:41:20 +0700

    another way to extract response

--- chatbot.py
+++ chatbot.py
@@ -5,30 +5,29 @@ from peft import PeftModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from prompt import make_prompt
 
-#BASE_MODEL = "VietAI/gpt-j-6B-vietnamese-news"
-#LORA_WEIGHTS = "tiendung/chat-gpt-j-6B-2e"
-BASE_MODEL = "VietAI/gpt-neo-1.3B-vietnamese-news"
-LORA_WEIGHTS = "tiendung/chat-gpt-neo-1.3B-alpaca-1e"
+BASE_MODEL = "VietAI/gpt-j-6B-vietnamese-news"
+LORA_WEIGHTS = "tiendung/chat-gpt-j-6B-2e"
+# BASE_MODEL = "VietAI/gpt-neo-1.3B-vietnamese-news"
+# LORA_WEIGHTS = "tiendung/chat-gpt-neo-1.3B-alpaca-1e"
 
 if torch.cuda.is_available():
     device = "cuda"
     device_map = {'': 0}
+    model = AutoModelForCausalLM.from_pretrained(BASE_MODEL, load_in_8bit=True, \
+        torch_dtype=torch.float16, device_map=device_map)
+    model = PeftModel.from_pretrained(model, LORA_WEIGHTS, \
+        torch_dtype=torch.float16, device_map=device_map)
 else:
     device = "cpu"
-    device_map = "auto"
+    model = AutoModelForCausalLM.from_pretrained(BASE_MODEL)
+    model = PeftModel.from_pretrained(model, LORA_WEIGHTS)
 
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
-
-model = AutoModelForCausalLM.from_pretrained(BASE_MODEL, load_in_8bit=True, \
-    torch_dtype=torch.float16, device_map=device_map)
-
-model = PeftModel.from_pretrained(model, LORA_WEIGHTS, \
-    torch_dtype=torch.float16, device_map=device_map)
 
 model.eval()
 if torch.__version__ >= "2": model = torch.compile(model) # tăng tốc
 
-def get_answer(q, max_new_tokens=320, skip_tl=False):
+def get_answer(q, max_new_tokens=196, skip_tl=False):
     input_ids = tokenizer(make_prompt(q), return_tensors="pt")["input_ids"].to(device)
     with torch.no_grad():
         gen_tokens = model.generate(
@@ -42,8 +41,13 @@ def get_answer(q, max_new_tokens=320, skip_tl=False):
             pad_token_id=tokenizer.eos_token_id,
         )
     origin_output = tokenizer.batch_decode(gen_tokens)[0]
-    output = origin_output.split("### Response")[1]
-    if output[0] == ":": output = output[1:]
+    output = origin_output.split("###")[2]
+    try:
+        k = output.index(":")
+        if k < 10: output = output[k+1:]
+    except:
+        output = output
+    # print(f"\n- - -{origin_output}- - -\n")
     return output.strip()
 
 def main():

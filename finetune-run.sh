4b5843f7ad5502f2ee7d6e5a094c951ab049b3e9
Tien Dung <dungtn@gmail.com>
2023-04-08 15:22:51 +0700

    Chuyển dữ liệu vào ./data/ cho gọn

--- finetune-run.sh
+++
@@ -1,4 +0,0 @@
-[ -f vi_merged.jsonl ] || cat vi*.jsonl > vi_merged.jsonl
-
-python3 finetune.py --data_path 'vi_merged.jsonl' --base_model 'VietAI/gpt-j-6B-vietnamese-news' \
-    --batch_size=128 --micro_batch_size 2 --cutoff_len 512 --num_epochs 1 --output_dir 'chat-gpt-j-6B-merged-1e'
